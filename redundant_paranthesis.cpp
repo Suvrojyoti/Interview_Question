fe9b4b4059ceb0a85a116af6e50916ea96561cca
Suvrojyoti <suvrojyotimandal1998@gmail.com>
2020-08-29 01:13:52 +0530

    29-08-20

---
+++ redundant_paranthesis.cpp
@@ -0,0 +1,102 @@
+/*
+Question: 
+
+(((abc))) --> abc
+(ab(c)) --> ab(c)
+(abc09%(c)) --> abc09%(c)
+ab(c) --> ab(c)
+(ab)c --> (ab)c
+abc(c)) → INVALID
+(abc)(def) --> (abc)(def)
+(abc)typ(def) --> (abc)typ(def)
+((abc)(def)) --> (abc)(def)
+
+Tags - Amazon
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    string expression;
+    cin>>expression;
+    string s=expression;
+    stack <int> exp_idx;
+    bool is_invalid = 0;
+    
+    /*if while iterating i get a ')' and the top of the stack is '(' the bracket pair is redundant, so we place '$' signs in those places
+    */
+    for(int i=0;i<expression.length();i++)
+    {
+        if(expression[i]==')')
+        {
+            if(!exp_idx.empty() and expression[exp_idx.top()]=='(')
+            {
+                expression[exp_idx.top()]='$';
+                expression[i]='$';
+                exp_idx.pop();
+            }
+            else
+            {
+            
+                while(!exp_idx.empty() and expression[exp_idx.top()]!='(')
+                    exp_idx.pop();
+                if(exp_idx.empty())
+                {
+                    i=expression.length();
+                    is_invalid=1;
+                }
+                else
+                {
+                    exp_idx.pop();
+                }
+            }
+            
+        }
+        else
+            exp_idx.push(i);
+        
+    }
+    
+    //special check for the removal of first and last bracket
+    
+    int front=0,back=expression.length()-1;
+    
+    while(front<expression.length() and expression[front]=='$')
+        front++;
+    while(back>=0 and expression[back]=='$')
+        back--;
+        
+    
+    if(front<back and expression[front]=='(' and expression[back]==')')
+    {
+        int i=front;
+        int bracket_balance=0;
+        while(i<back)
+        {
+            if(expression[i]=='(')
+                bracket_balance++;
+            else if(expression[i]==')')
+            {
+                bracket_balance--;
+                if(bracket_balance==0)
+                    break;
+            }
+            i++;
+        }
+        if(i==back)
+            expression[front]=expression[back]='$';
+    }
+    // special check ends
+    
+    if(is_invalid)
+        cout<<"INVALID EXPRESSION";
+    else
+        for(auto it:expression)
+            if(it!='$') cout<<it;
+    
+    return 0;    
+}
